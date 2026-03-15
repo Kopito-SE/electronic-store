2308fe1d84a7595c0e264ef9834592190e22a0eb
freeman03-del <beckhamjunior56@gmail.com>
2026-03-16 00:04:04 +0300

    Improved homepage styling and added navbar component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Load navbar on all pages
+fetch("navbar.html")
+.then(response => response.text())
+.then(data => {
+    const navbarContainer = document.getElementById("navbar");
+    if (navbarContainer) {
+        navbarContainer.innerHTML = data;
+    }
+});
 /**
  * KEPKOF Electronics - Main JavaScript
  * Optimized for performance, accessibility, and mobile responsiveness
